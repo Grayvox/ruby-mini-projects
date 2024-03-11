f923472748a51ba586209bdff725b840de5bd14b
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-11 19:14:37 -0400

    Add first RSpec test
    Checks for base functionality in lowercase letters

--- caeser_cipher/spec/caeser_cipher_spec.rb
+++ caeser_cipher/spec/caeser_cipher_spec.rb
@@ -1,1 +1,12 @@
-# To be filled
+require 'spec_helper'
+require_relative '../caeser_cipher.rb'
+
+RSpec.describe 'Caeser Cipher' do
+
+    context 'with lowercase letters' do
+        it 'returns the correct output' do
+            expect(caeser_cipher('aaa', 1)).to eq('bbb')
+        end
+    end
+    
+end
