899d4ee533ae980a4a3ee0a57f1cbdbd306d4658
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-11 19:19:10 -0400

    Add mixed letters test

--- caeser_cipher/spec/caeser_cipher_spec.rb
+++ caeser_cipher/spec/caeser_cipher_spec.rb
@@ -14,5 +14,11 @@ RSpec.describe 'Caeser Cipher' do
             expect(caeser_cipher('YYY', 1)).to eq('ZZZ')
         end
     end
+
+    context 'with both upper and lowercase letters' do
+        it 'returns the correct output' do
+            expect(caeser_cipher('rRr', 2)).to eq('tTt')
+        end
+    end
     
 end
