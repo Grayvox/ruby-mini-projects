0a60b89b0f51e6d9df0a1eb0565e6b33a182709f
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-11 18:57:37 -0400

    Add ASCII code conversion and ciphering
    This does NOT include wrapping Z to A yet

--- caeser_cipher/caeser_cipher.rb
+++ caeser_cipher/caeser_cipher.rb
@@ -5,6 +5,13 @@ def caeser_cipher(string, shift = 0)
     if string =~ /[^a-z^A-Z^\s]/
         return 'Error! All elements must be alphabetical.' 
     end
+
+    stringArray = string.chars.map do |element| 
+        elementShift = element.ord + shift
+        elementShift.chr
+    end
+    shiftedStr = stringArray.join
+    shiftedStr
 end
 
 p caeser_cipher('zzz ZZZ', 5)
