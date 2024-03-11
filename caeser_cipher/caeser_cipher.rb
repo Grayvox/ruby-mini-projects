865af4aabf49b925e232c33168a94c7285578652
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-11 18:59:41 -0400

    Add wrapping from Z to A
    * Adds functionality to wrap letters from Z back to A to continue on
    
    * Fixes a bug that deletes whitespace

--- caeser_cipher/caeser_cipher.rb
+++ caeser_cipher/caeser_cipher.rb
@@ -8,10 +8,19 @@ def caeser_cipher(string, shift = 0)
 
     stringArray = string.chars.map do |element| 
         elementShift = element.ord + shift
+
+        if element.ord == 32
+            next ' '
+        elsif elementShift > 90 && element == element.upcase
+            difference = elementShift - 90
+            elementShift = 64 + difference
+        elsif elementShift > 122 && element == element.downcase
+            difference = elementShift - 122
+            elementShift = 96 + difference
+        end
+
         elementShift.chr
     end
     shiftedStr = stringArray.join
     shiftedStr
 end
-
-p caeser_cipher('zzz ZZZ', 5)
