6dce1b2402ee7f149f6a96c7836581c909f8bab4
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-15 17:18:37 -0400

    Add base RSpec tests for substrings

--- sub_strings/spec/sub_strings_spec.rb
+++ sub_strings/spec/sub_strings_spec.rb
@@ -1,1 +1,12 @@
-# To be filled
+require 'spec_helper'
+require_relative '../sub_strings.rb'
+
+RSpec.describe 'Substrings' do
+
+    context 'with single word and single letter' do
+        it 'returns the correct amount of substrings found' do
+            expect(substrings('Hello', ['o'])).to eq({'o' => 1})
+        end
+    end
+
+end
