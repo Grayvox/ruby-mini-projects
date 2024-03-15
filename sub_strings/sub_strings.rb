de43302f45c7756b9d5c64142d4692f26598705a
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-15 17:14:55 -0400

    Finish sub strings solution v1
    May be subject to change in the future, but it works!

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,13 +1,15 @@
 require 'pry'
 require 'pry-byebug'
 
-def substrings(word, dictionary = [])
+def substrings(string, dictionary = [])
     results = {}
+    lower_string = string.downcase
 
     dictionary.each do |element|
-        
+        lower_element = element.downcase
+        if lower_string.scan(/#{lower_element}/).count <= 0 then next end
 
-        
+        results[element] = lower_string.scan(/#{lower_element}/).count
     end
 
     results
