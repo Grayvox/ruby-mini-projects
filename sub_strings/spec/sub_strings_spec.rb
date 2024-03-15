a86128f0cdfcb7ac58835edc624a2ea1351db5f9
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-15 17:35:40 -0400

    Add more tests to substrings

--- sub_strings/spec/sub_strings_spec.rb
+++ sub_strings/spec/sub_strings_spec.rb
@@ -9,4 +9,22 @@ RSpec.describe 'Substrings' do
         end
     end
 
+    context 'with single word and multiple letters together' do
+        it 'returns the correct amount of substrings found' do
+            expect(substrings('Hello', ['llo', 'll', 'He'])).to eq({'llo' => 1, 'll' => 1, 'He' => 1})
+        end
+    end
+
+    context 'with sentences' do
+        it 'returns the correct amount of substrings found' do
+            expect(substrings('Hello, world! Nice to meet you!', ['o', 'ee', 'to'])).to eq({'o' => 4, 'ee' => 1, 'to' => 1})
+        end
+    end
+
+    context 'with incorrect capitalizations' do
+        it 'still returns the correct substring count' do
+            expect(substrings('Hello, world! Nice to meet you!', ['O', 'Ee', 'TO'])).to eq({'O' => 4, 'Ee' => 1, 'TO' => 1})
+        end
+    end
+    
 end
