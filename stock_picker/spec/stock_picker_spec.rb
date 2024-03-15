c5f4046230bb1e4f682433367b2838361a604fa8
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-15 18:19:21 -0400

    STOCK PICKER: Add base code

--- stock_picker/spec/stock_picker_spec.rb
+++ stock_picker/spec/stock_picker_spec.rb
@@ -1,1 +1,12 @@
-# To be filled
+require 'spec_helper'
+require_relative '../stock_picker.rb'
+
+RSpec.describe 'Stock Picker' do
+
+    context '' do
+        it '' do
+            expect(stock_picker()).to eq()
+        end
+    end
+
+end 
---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'pry-byebug'
+
+def stock_picker(prices = []) 
+    
+end
