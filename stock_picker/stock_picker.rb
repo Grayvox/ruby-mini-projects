3f672cbf0c66e73bfd5edad9c0ad91b4930a0d45
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-18 20:12:56 -0400

    Finished stock picker

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -2,5 +2,30 @@ require 'pry'
 require 'pry-byebug'
 
 def stock_picker(prices = []) 
-    
+    results = []
+    buy = 0
+    sell = 0
+    buy_index = 0
+    sell_index = 0
+
+    prices.each_with_index do |a, a_index|
+
+        prices.each_with_index do |b, b_index|
+            if b_index <= a_index then next end
+
+            if b - a > sell - buy
+                buy = a
+                buy_index = a_index
+                sell = b
+                sell_index = b_index
+            end
+
+        end
+
+    end 
+
+    results.push(buy_index)
+    results.push(sell_index)
 end
+
+p stock_picker([18, 19, 2, 4, 6, 18, 1])
