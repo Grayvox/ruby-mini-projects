aa4f138d9a643a8d4b6892017b4a8950208ff474
Grayvox <144566632+Grayvox@users.noreply.github.com>
2024-03-22 18:26:56 -0400

    Finish bubble sort project

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -2,5 +2,22 @@ require 'pry'
 require 'pry-byebug'
 
 def bubble_sort(array = [])
+
+    array.each do ||
+
+        array.each_with_index do |element, index|
+            next_el = array[index + 1]
     
+            if !next_el then next end
+    
+            if element > next_el
+                array[index], array[index + 1] = array[index + 1], array[index]
+            end
+    
+        end
+
+    end
+
 end
+
+p bubble_sort([8, 9, 4, 7, 1, 3, 6, 2])
